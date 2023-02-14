dcff1dfdfaec8598b64a5e71f9fb72940c029925
Don Welch <dwelch@nait.ca>
2023-02-14 09:47:39 -0700

    Intro to Razor pages

---
+++ WebAppSolution/WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public string OwnerName { get; set; } = "default";
+        public void OnGet()
+        {
+            // this method is call on your behave when the web page is first
+            //    created
+            // it is called as the web page is being constructed by the system
+            // it CAN be call by other code in the rest of this page mdoel class
+            OwnerName = "Don";
+        }
+    }
+}
